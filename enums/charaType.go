262b89e531e783f189d4f36bca43bcfcc1b7de27
FairPlay137 <danielws424@gmail.com>
2020-11-02 17:16:16 -0800

    uggggggggggggggggggh i was off by one

--- enums/charaType.go
+++ enums/charaType.go
@@ -25,7 +25,7 @@ const (
 	CharaTypeTikal
 	CharaTypeMephiles
 	CharaTypePSISilver
-	CharaTypeAmitieAmy = iota + 301000 - 21 // 300021 is not the correct ID for Amitie Amy.
+	CharaTypeAmitieAmy = iota + 301000 - 22 // 300021 is not the correct ID for Amitie Amy.
 	CharaTypeGothicAmy
 	CharaTypeHalloweenShadow
 	CharaTypeHalloweenRouge
