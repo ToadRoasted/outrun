036aee6e0c1c2a7882cae886a8c2f52caa59f48d
FairPlay137 <danielws424@gmail.com>
2020-12-18 08:03:08 -0800

    Revise suspension notices

--- localizations/localizedStrings.go
+++ localizations/localizedStrings.go
@@ -16,7 +16,7 @@ var LocalizedStrings = map[string]map[string]string{
 			"Twitter: https://twitter.com/runnersrevival\n" +
 			"Discord: https://discord.gg/T5ytR6T",
 		"DefaultRewardLabel": "A gift from the Revival Team.",
-		"DeviceSuspensionNotice": "This device has been blocked from accessing the Sonic Runners Revival game server, likely due to repeated incidents of cheating.\n\n" +
+		"DeviceSuspensionNotice": "This device has been blocked from accessing the Sonic Runners Revival game server.\n\n" +
 			"If you feel this is in error, please get in touch!\n" +
 			"Twitter: https://twitter.com/runnersrevival\n" +
 			"Discord: https://discord.gg/T5ytR6T",
@@ -28,32 +28,31 @@ var LocalizedStrings = map[string]map[string]string{
 		"QuickLeagueHighRankingRewardLabel":  "A reward for getting the following position in the Runners' League Timed Mode High Score Ranking: %v.",
 		"QuickLeaguePromotionRewardLabel":    "Runners' League Promotion Reward. Timed Mode.",
 		"QuickLeagueTotalRankingRewardLabel": "A reward for getting the following position in the Runners' League Timed Mode Total Score Ranking: %v.",
-		"SuspensionNotice_Temporary": "The account on this device has been temporarily suspended from Sonic Runners Revival until the following date: %s\n" +
-			"Reason: %s\n\n" +
+		"SuspensionNotice_Temporary": "The account on this device has been temporarily suspended from Sonic Runners Revival for %s\n" +
+			"You will be able to play the game again at: %s\n\n" +
 			"If you feel this is in error, please get in touch!\n" +
 			"Twitter: https://twitter.com/runnersrevival\n" +
 			"Discord: https://discord.gg/T5ytR6T",
-		"SuspensionNotice_Permanent": "The account on this device has been permanently banned from Sonic Runners Revival.\n" +
-			"Reason: %s\n\n" +
+		"SuspensionNotice_Permanent": "The account on this device has been permanently banned from Sonic Runners Revival for %s\n\n" +
 			"If you feel this is in error, please get in touch!\n" +
 			"Twitter: https://twitter.com/runnersrevival\n" +
 			"Discord: https://discord.gg/T5ytR6T",
-		"SuspensionReason_0": "(no reason specified)",
-		"SuspensionReason_1": "Cheating",
-		"SuspensionReason_2": "Packet manipulation",
-		"SuspensionReason_3": "Account given away or sold",
-		"SuspensionReason_4": "Reserved for future use (Reason ID 4)",
-		"SuspensionReason_5": "Reserved for future use (Reason ID 5)",
-		"SuspensionReason_6": "Reserved for future use (Reason ID 6)",
-		"SuspensionReason_7": "Reserved for future use (Reason ID 7)",
-		"SuspensionReason_8": "Reserved for future use (Reason ID 8)",
-		"SuspensionReason_9": "Reserved for future use (Reason ID 9)",
+		"SuspensionReason_0": "an unspecified reason.",
+		"SuspensionReason_1": "cheating in-game.\nPlease don't do that!",
+		"SuspensionReason_2": "packet manipulation or exploiting a server glitch.\nPlease stop.",
+		"SuspensionReason_3": "an unknown reason (RID 3).",
+		"SuspensionReason_4": "an unknown reason (RID 4).",
+		"SuspensionReason_5": "an unknown reason (RID 5).",
+		"SuspensionReason_6": "an unknown reason (RID 6).",
+		"SuspensionReason_7": "an unknown reason (RID 7).",
+		"SuspensionReason_8": "an unknown reason (RID 8).",
+		"SuspensionReason_9": "an unknown reason (RID 9).",
 		"UpdateGameNotice": "Please update Sonic Runners Revival; this version is no longer supported on Sonic Runners Revival!\n\n" +
 			"Our website: https://sonic.runner.es/\n" +
 			"Twitter: https://twitter.com/runnersrevival\n" +
 			"Discord: https://discord.gg/T5ytR6T",
 		"WatermarkTicker_1": "This server is powered by [ff0000]Outrun for Revival!",
-		"WatermarkTicker_2": "ID: [0000ff]%s",
+		"WatermarkTicker_2": "User ID: [0000ff]%s",
 		"WatermarkTicker_3": "High score (Timed Mode): [0000ff]%v",
 		"WatermarkTicker_4": "High score (Story Mode): [0000ff]%v",
 		"WatermarkTicker_5": "Total distance ran (Story Mode): [0000ff]%v",
