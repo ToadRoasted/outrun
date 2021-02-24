f88a901951135a9cdcde890f4a5427eed302f774
ToadRoasted <59739959+ToadRoasted@users.noreply.github.com>
2021-02-24 06:57:19 -0500

    Update randomChaoItemList.go

--- consts/randomChaoItemList.go
+++ consts/randomChaoItemList.go
@@ -16,30 +16,30 @@ var RandomChaoWheelCharacterPrizes = map[string]float64{
 	// characterID: load
 	// Hopefully this should sum up to 100 just for
 	// simplicity, but it shouldn't be a requirement.
-	enums.CTStrSonic:           0.7, // Initial character
-	enums.CTStrTails:           0.7, // Obtained in story mode
-	enums.CTStrKnuckles:        0.7, // Obtained in story mode
-	enums.CTStrAmy:             1.0,
-	enums.CTStrBig:             0.7,
+	//enums.CTStrSonic:           0.7, // Initial character
+	//enums.CTStrTails:           0.7, // Obtained in story mode
+	//enums.CTStrKnuckles:        0.7, // Obtained in story mode
+	//enums.CTStrAmy:             1.0,
+	//enums.CTStrBig:             0.7,
 	//enums.CTStrBlaze:           1.0, // Revival Event (Sonic Rush)
-	enums.CTStrCharmy:          1.0,
-	enums.CTStrCream:           0.7,
-	enums.CTStrEspio:           1.0,
+	//enums.CTStrCharmy:          1.0,
+	//enums.CTStrCream:           0.7,
+	//enums.CTStrEspio:           1.0,
 	//enums.CTStrMephiles:        0.0, // Revival Event
-	enums.CTStrOmega:           0.5,
+	//enums.CTStrOmega:           0.5,
 	//enums.CTStrPSISilver:       0.0, // Revival Event
-	enums.CTStrRouge:           0.5,
-	enums.CTStrShadow:          0.5,
-	enums.CTStrSilver:          0.7,
+	//enums.CTStrRouge:           0.5,
+	//enums.CTStrShadow:          0.5,
+	//enums.CTStrSilver:          0.7,
 	//enums.CTStrSticks:          0.0, // Revival Event
-	enums.CTStrTikal:           1.5, // Event (Sonic Adventure)
-	enums.CTStrVector:          1.0,
-	enums.CTStrWerehog:         1.5, // Revival Event
+	//enums.CTStrTikal:           1.5, // Event (Sonic Adventure)
+	//enums.CTStrVector:          1.0,
+	//enums.CTStrWerehog:         1.5, // Revival Event
 	//enums.CTStrClassicSonic:    0.7, // Event (Birthday)
 	//enums.CTStrMetalSonic:      0.0, // Revival Event
 	
 	// The below characters shouldn't be activated until event characters are fixed!
-	//enums.CTStrAmitieAmy:       1.0, // Event (Puyo Puyo Quest)
+	enums.CTStrAmitieAmy:       30.0, // Event (Puyo Puyo Quest)
 	//enums.CTStrGothicAmy:       0.0, // Revival Event
 	//enums.CTStrHalloweenShadow: 1.0, // Event (Halloween)
 	//enums.CTStrHalloweenRouge:  1.0, // Event (Halloween)
