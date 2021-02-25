11ea7563fc951a3ad3fc1cb52e7043b5d829aa6c
jesstendo <79614264+jesstendo@users.noreply.github.com>
2021-02-24 20:49:13 -0500

    Update leaderboard.go

--- responses/leaderboard.go
+++ responses/leaderboard.go
@@ -1,11 +1,11 @@
 package responses
 
 import (
-	"github.com/Mtbcooler/outrun/logic/conversion"
-	"github.com/Mtbcooler/outrun/netobj"
-	"github.com/Mtbcooler/outrun/obj"
-	"github.com/Mtbcooler/outrun/obj/constobjs"
-	"github.com/Mtbcooler/outrun/responses/responseobjs"
+	"github.com/ToadRoasted/outrun/logic/conversion"
+	"github.com/ToadRoasted/outrun/netobj"
+	"github.com/ToadRoasted/outrun/obj"
+	"github.com/ToadRoasted/outrun/obj/constobjs"
+	"github.com/ToadRoasted/outrun/responses/responseobjs"
 	"github.com/jinzhu/now"
 )
 
