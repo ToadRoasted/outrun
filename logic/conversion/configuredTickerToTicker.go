6178d2a0d09a852248ad6a4e78a8112dd378bcce
ToadRoasted <59739959+ToadRoasted@users.noreply.github.com>
2021-02-24 20:34:57 -0500

    Update configuredTickerToTicker.go

--- logic/conversion/configuredTickerToTicker.go
+++ logic/conversion/configuredTickerToTicker.go
@@ -3,8 +3,8 @@ package conversion
 import (
     "time"
 
-    "github.com/Mtbcooler/outrun/config/infoconf"
-    "github.com/Mtbcooler/outrun/obj"
+    "github.com/ToadRoasted/outrun/config/infoconf"
+    "github.com/ToadRoasted/outrun/obj"
     "github.com/jinzhu/now"
 )
 
